a67281d411c00f0c143a4961f82ffe926ace13ce
apetros <apetros@pm.me>
2022-01-05 16:40:26 +0200

    Updated 3.40

--- src/usr_dctl_models.f90
+++ src/usr_dctl_models.f90
@@ -1,6 +1,3 @@
-!> @file
-!> @brief associates the name of the DCTL with the actual subroutine
-
 !> @brief associates the name of the DCTL with the actual subroutine
 !> @details .
 subroutine assoc_dctl_ptr(modelname,dctl_ptr)
@@ -10,32 +7,13 @@ subroutine assoc_dctl_ptr(modelname,dctl_ptr)
    implicit none
 
    character(len=20), intent(inout):: modelname
-   character(len=25):: modelname5
    procedure(dctl), pointer, intent(out) :: dctl_ptr
-   ! external dctl_OLTC2
-   integer(C_INTPTR_T) :: p_USERFUNC
-   integer(BOOL) :: free_status
-   integer i, ret
+   ! external dctl_line_prot
 
-   if(modelname(1:5)=='dctl_')then
-      modelname5=modelname
-   else
-      modelname5='dctl_'//modelname
-   endif
-
-#if defined __INTEL_COMPILER && (defined _WIN64 || defined _WIN32)
-   do i=1,dll_handleno
-      if (dll_handle(i) .ne. NULL) then
-         p_USERFUNC = GetProcAddress (hModule=dll_handle(i), lpProcName=trim(modelname5)//C_NULL_CHAR)
-         if (p_USERFUNC .ne. NULL) then
-            call C_F_PROCPOINTER (TRANSFER(p_USERFUNC, C_NULL_FUNPTR), dctl_ptr)
-            return
-         endif
-      endif
-   enddo
-#endif
-
-   select case (modelname5)
+   select case (modelname)
+       
+   !    case('dctl_line_prot')
+   !      dctl_ptr=>dctl_line_prot
 
    case default
 
